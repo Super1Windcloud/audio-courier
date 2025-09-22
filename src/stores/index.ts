72773cfa6c30d4eb9a0cd2a1c241f9d8be275573
SuperWindCloud <19338823723@163.com>
2025-09-22 22:04:26 +0800

    update

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,41 +2,41 @@ import { create } from "zustand";
 import { ModelOption } from "@/types/llm.ts";
 
 interface AppStateStore {
-  currentSelectedModel: ModelOption;
-  updateCurrentSelectedModel: (target: ModelOption) => void;
+	currentSelectedModel: ModelOption;
+	updateCurrentSelectedModel: (target: ModelOption) => void;
 
-  currentAudioChannel: string;
-  updateCurrentAudioChannel: (target: string) => void;
+	currentAudioChannel: string;
+	updateCurrentAudioChannel: (target: string) => void;
 
-  llmPrompt: string;
-  updateLLMPrompt: (target: string) => void;
-  currentQuestion: string;
-  updateQuestion: (target: string) => void;
+	llmPrompt: string;
+	updateLLMPrompt: (target: string) => void;
+	currentQuestion: string;
+	updateQuestion: (target: string) => void;
 
-  isStartScrollToBottom: boolean;
-  updateScrollToBottom: (target: boolean) => void;
+	isStartScrollToBottom: boolean;
+	updateScrollToBottom: (target: boolean) => void;
 }
 
 const useAppStateStore = create<AppStateStore>((set) => ({
-  currentSelectedModel: "siliconflow", // 默认模型
-  updateCurrentSelectedModel: (target: ModelOption) =>
-    set({ currentSelectedModel: target }),
+	currentSelectedModel: "siliconflow", // 默认模型
+	updateCurrentSelectedModel: (target: ModelOption) =>
+		set({ currentSelectedModel: target }),
 
-  currentAudioChannel: "",
-  updateCurrentAudioChannel: (target: string) =>
-    set({ currentAudioChannel: target }),
-  llmPrompt: import.meta.env.VITE_PROMPT || "",
-  updateLLMPrompt: (target: string) => {
-    set({ llmPrompt: target });
-  },
-  currentQuestion: "",
-  updateQuestion: (target: string) => {
-    set({ currentQuestion: target });
-  },
-  isStartScrollToBottom: false,
-  updateScrollToBottom: (target: boolean) => {
-    set({ isStartScrollToBottom: target });
-  },
+	currentAudioChannel: "",
+	updateCurrentAudioChannel: (target: string) =>
+		set({ currentAudioChannel: target }),
+	llmPrompt: import.meta.env.VITE_PROMPT || "",
+	updateLLMPrompt: (target: string) => {
+		set({ llmPrompt: target });
+	},
+	currentQuestion: "",
+	updateQuestion: (target: string) => {
+		set({ currentQuestion: target });
+	},
+	isStartScrollToBottom: false,
+	updateScrollToBottom: (target: boolean) => {
+		set({ isStartScrollToBottom: target });
+	},
 }));
 
 export default useAppStateStore;
