fa6ae42713bc02e61299a2f8492c22ffe9febbb5
SuperWindCloud <19338823723@163.com>
2025-10-18 13:06:28 +0800

    update

--- src-tauri/src/constant.rs
+++
@@ -1,7 +0,0 @@
-pub const APP_ID: &str = "10787ef6";
-pub const ACCESS_KEY_ID: &str = "85e042b9ee54869eb38df6d4cf8cca76";
-pub const ACCESS_KEY_SECRET: &str = "N2QyNDBkMTAxYTBjNjVhYzZmOWZjOTky";
-
-pub const TARGET_SAMPLE_RATE: &str = "16000";
-
-pub const RESAMPLE_RATE: u32 = 16000;
