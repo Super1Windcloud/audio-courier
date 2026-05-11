c3ecae08450511ba2617a24c3a46314de4f25618
SuperWindCloud <19338823723@163.com>
2026-05-11 18:50:48 +0800

    fix: Add type for message props in MessageItem

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,7 +1,7 @@
 import type React from "react";
 import { cn } from "@/lib/utils";
-import { MarkdownMessage } from "./MarkdownMessage";
 import type { Message } from "./ChatContainer";
+import { MarkdownMessage } from "./MarkdownMessage";
 
 interface MessageItemProps {
 	message: Message;
